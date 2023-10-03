6f25993ea7fe8a02088196c556c86862ac9b8dfc
Fawcett <fawcettt@student.douglascollege.ca>
2023-10-03 16:19:34 -0700

    Feat: Model, controller updated, trevorPage created

--- src/main/java/com/csis3275/controller_group3/Controller_group3.java
+++ src/main/java/com/csis3275/controller_group3/Controller_group3.java
@@ -1,16 +1,34 @@
 package com.csis3275.controller_group3;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.csis3275.model_group3.*;
 
 @Controller
 public class Controller_group3 {
 
+	
+	String studentName_tfa = "Trevor Fawcett";
+	String imgLink_tfa = "/images/profilepicture.png";
+	
+	String bioInfo_tfa = "Studied at Douglas college since Winter 2018. Currently enrolled in Comp Study & Information Systems diploma."
+			+ "Age 26. Have some experience programming in HTML, CSS, Javascript, PHP, Java, Android development.";
+
+	
 
 	@GetMapping("/")
 	public String showMainPage() {
 
 		return "home";
 	}
-
+	
+	@GetMapping("/trevor")
+	public String showTrevorPage(Model model) {
+		Model_group3 object = new Model_group3(studentName_tfa, imgLink_tfa,bioInfo_tfa);
+		model.addAttribute("trevor", object);
+		
+		return "trevorPage";
+	}
 }
--- src/main/java/com/csis3275/model_group3/Model_group3.java
+++ src/main/java/com/csis3275/model_group3/Model_group3.java
@@ -2,13 +2,43 @@ package com.csis3275.model_group3;
 
 public class Model_group3 {
 
-	String studentName;
-	String imgLink;
-	String bioInfo;
+	public String studentName;
+	public String imgLink;
+	public String bioInfo;
 	
 	public Model_group3(String sName, String link, String bio) {
 		this.studentName = sName;
 		this.imgLink = link;
 		this.bioInfo = bio;
 	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public String getImgLink() {
+		return imgLink;
+	}
+
+	public void setImgLink(String imgLink) {
+		this.imgLink = imgLink;
+	}
+
+	public String getBioInfo() {
+		return bioInfo;
+	}
+
+	public void setBioInfo(String bioInfo) {
+		this.bioInfo = bioInfo;
+	}
+
+
+
+
 }
+
+
